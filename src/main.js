109ca4bd10aeee7d29fadb4962b39c68a732ad58
doe0003p <doe0003p@belgium.fhm.de>
2025-07-04 08:42:55 +0200

    tested

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import promptConfig from './prompt.js';
 import scaffold from './scaffold.js';
 
 export default async function () {
-  console.log(chalk.yellow('\n🧪 Welcome to testla-screenplay init\n'));
+  console.log(chalk.yellow(`\n🧪 Welcome to testla-screenplay init\n`));
   const answers = await promptConfig();
   await scaffold(answers);
 }
